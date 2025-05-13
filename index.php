f9bf47189ab159ee7d73d8e4174782a632e935f0
Dalc26 <alessandro14coronado@gmail.com>
2025-05-12 18:29:43 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+<body bgcolor="##251FE7" text="#FFFFFF">
+	<h1 align="center">Departamentos y cabeceras de Guatemala </h1>
+	<?php
+	echo "Petén: Flores <br><br>";	
+	echo "Izabal: Puerto Barrios <br><br>";
+	echo "Alta Verapaz: Cobán <br><br>";
+	echo "Quiché: Santa Cruz del Quiché<br><br>";
+	echo "Huehuetenango: Huehuetenango<br><br>";
+	echo "Escuintla: Escuintla<br><br>";
+	echo "San Marcos: San Marcos<br><br>";
+	echo "Jutiapa: Jutiapa<br><br>";
+	echo "Baja Verapaz:Salamá<br><br>";
+	echo "Santa Rosa: Cuilapa<br><br>";
+	echo " Zacapa:  Zacapa<br><br>";
+	echo "Suchitepéquez:  Mazatenango<br><br>";
+	echo "Chiquimula:Chiquimula<br><br>";
+	echo " Guatemala:  Guatemala<br><br>";
+	echo " Jalapa: Jalapa<br><br>";
+	echo "Chimaltenango: Chimaltenango<br><br>";
+	echo "Quetzaltenango: Quetzaltenango<br><br>";
+	echo "El Progreso: El Progreso<br><br>"; 
+	echo "Retalhuleu: Retalhuleu<br><br>"; 
+	echo "Sololá: Sololá<br><br>"; 
+	echo " Totonicapán:  Totonicapán<br><br>";
+	echo " Sacatepéquez: Antigua Guatemala<br><br>";
+	?>
+</body>
+</html>
